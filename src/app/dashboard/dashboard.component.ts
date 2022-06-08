38fc2d436a4bfd33696c2a88e1152888fefb8e4e
unknown <cheikhibra1988@gmail.com>
2022-06-08 13:01:45 +0200

    pull

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as Chartist from 'chartist';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-dashboard',
@@ -146,5 +149,60 @@ export class DashboardComponent implements OnInit {
       //start animation for the Emails Subscription Chart
       this.startAnimationForBarChart(websiteViewsChart);
   }
+
+/*----------------------GENERER PDF------------------------- */
+  generatePdf(){
+    const documentDefinition = { content: 'This is an sample PDF printed with pdfMakeThis is an sample PDF printed with pdfMakeThis is an sample PDF printed with pdfMakeThis is an sample PDF printed with pdfMakeThis is an sample PDF printed with pdfMakeThis is an sample PDF printed with pdfMakeThis is an sample PDF printed with pdfMakeThis is an sample PDF printed with pdfMake' };
+    pdfMake.createPdf(documentDefinition).open();
+   }
+
+   /*getDocumentDefinition() {
+    
+    sessionStorage.setItem('resume', JSON.stringify(this.resume));<br> ​    
+     return {
+       content: [
+         {
+           text: 'RESUME',
+           bold: true,
+           fontSize: 20,
+           alignment: 'center',
+           margin: [0, 0, 0, 20]
+         },
+         {
+           columns: [
+             [{
+               text: this.resume.name,
+               style: 'name'
+             },
+             {
+               text: this.resume.address
+             },
+             {
+               text: 'Email : ' + this.resume.email,
+             },
+             {
+               text: 'Contant No : ' + this.resume.contactNo,
+             },
+             {
+               text: 'GitHub: ' + this.resume.socialProfile,
+               link: this.resume.socialProfile,
+               color: 'blue',
+             }
+             ],
+             [
+               // Document definition for Profile pic
+             ]
+           ]
+         }],
+         styles: {
+           name: {
+             fontSize: 16,
+             bold: true
+           }
+         }
+     };
+   }*/
+
+
 
 }
